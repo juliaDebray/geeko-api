03f5414e5a77bbb28ac80eab331b736e9bd2990a
Julia DEBRAY <julia.debray@gmail.com>
2022-01-18 13:59:50 +0100

    Ajout de fixtures

--- src/DataFixtures/ApiFixtures.php
+++ src/DataFixtures/ApiFixtures.php
@@ -2,8 +2,12 @@
 
 namespace App\DataFixtures;
 
-
 use App\Entity\Administrator;
+use App\Entity\Customer;
+use App\Entity\Ingredient;
+use App\Entity\IngredientType;
+use App\Entity\PotionType;
+use App\Entity\Tool;
 use App\Service\UserService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -19,6 +23,7 @@ class ApiFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+        /** Créer un administrateur */
         $admin = new Administrator();
 
         $admin
@@ -28,6 +33,58 @@ class ApiFixtures extends Fixture
         $admin = $this->userService->makeUser($admin, ['ROLE_ADMIN'], 'validated');
 
         $manager->persist($admin);
+
+        /** Créer un outil d'alchimie */
+        $tool = new Tool();
+
+        $tool
+            ->setImage('default')
+            ->setName('Cornue');
+
+        $manager->persist($tool);
+
+        /** Créer un utilisateur */
+        $customer = new Customer();
+
+        $customer
+            ->setEmail('ayamoure@example.com')
+            ->setPassword('Pa$$w0rd')
+            ->setAlchemistLevel('1')
+            ->setPseudo('ayamoure')
+            ->setAlchemistTool($tool);
+
+        $customer = $this->userService->makeUser($customer, ['ROLE_CUSTOMER'], 'validated');
+
+        $manager->persist($customer);
+
+        /** Créer un type d'ingrédient */
+        $ingredientType = new IngredientType();
+
+        $ingredientType
+            ->setName('champignon');
+
+        $manager->persist($ingredientType);
+
+        /** Créer 5 ingrédients */
+        for ($i = 0; $i < 5; $i++)
+        {
+            $ingredient = new Ingredient();
+            $ingredient->setName('ingredient' . $i+1);
+            $ingredient->setImage('default');
+            $ingredient->setType($ingredientType);
+            $manager->persist($ingredient);
+        }
+
+        /** Créer un type de potion */
+        $potionType = new PotionType();
+
+        $potionType
+            ->setName('soin')
+            ->setImage('default');
+
+        $manager->persist($potionType);
+
+        /** Envoie les données en base de données */
         $manager->flush();
     }
 }
