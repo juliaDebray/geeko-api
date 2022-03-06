e50004b90916f7687ff96fcedcfd29277e89f0dd
juliadebray <63407162+juliadebray@users.noreply.github.com>
2022-03-06 13:17:45 +0100

    geeko-76 : personnalisation des GET/GET ALL potion selon le statut (#25)
    * personnalisation des GET/GET ALL potion selon le statut
    
    * correctifs sur le data provider de potion

--- src/Constants/ErrorMessage.php
+++ src/Constants/ErrorMessage.php
@@ -4,8 +4,9 @@ namespace App\Constants;
 
 class ErrorMessage
 {
-    const TOOL_NOT_FOUND = "Outil indisponible";
-    const POTION_TYPE_NOT_FOUND = "Le type de la potion n'existe pas";
-    const INGREDIENT_TYPE_NOT_FOUND = "Ce type d'ingrédient n'existe pas";
     const INGREDIENT_NOT_FOUND = "Ingrédient indisponible";
+    const INGREDIENT_TYPE_NOT_FOUND = "Ce type d'ingrédient n'existe pas";
+    const POTION_NOT_FOUND = "Potion indisponible";
+    const POTION_TYPE_NOT_FOUND = "Le type de la potion n'existe pas";
+    const TOOL_NOT_FOUND = "Outil indisponible";
 }
